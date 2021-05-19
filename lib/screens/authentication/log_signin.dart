877deb63e8d967a052c607247e7665912ea52fd0
shruti-santosh <shrutisantosh99@yahoo.com>
2021-05-19 18:48:24 +0530

    made screens for authentication

--- lib/screens/authentication/log_signin.dart
+++ lib/screens/authentication/log_signin.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-class LogSignin extends StatelessWidget {
+class Home extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context){
+    return Container(
+      child: Text('home'),
+    );
   }
 }
---
+++ lib/screens/wrapper/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:stealthzero/screens/authentication/log_signin.dart';
+
+class Wrapper extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
