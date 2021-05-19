0245f592118b50054f6727cf01803feb6650ad8d
shruti-santosh <shrutisantosh99@yahoo.com>
2021-05-19 22:44:32 +0530

    Added some code to authentication and wrapper

---
+++ lib/screens/authentication/signin.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:stealthzero/screens/authentication/login.dart';
+import 'package:stealthzero/services/auth.dart';
+class SignIn extends StatefulWidget {
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  static Color col1 = const Color(0xff557089);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white ,
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0 ),
+        child: Text('Sign In',
+        style: TextStyle(color:col1)
+        ),
+
+      ),
+    );
+  }
+}
--- lib/screens/wrapper/wrapper.dart
+++ lib/screens/wrapper/wrapper.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:stealthzero/screens/authentication/log_signin.dart';
+import 'package:stealthzero/screens/authentication/login.dart';
 
 class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return Authenticate();
   }
 }
