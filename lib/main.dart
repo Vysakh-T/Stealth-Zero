6544654c73ceef23a91e41dd57e7171614d4be11
shruti-santosh <shrutisantosh99@yahoo.com>
2021-05-21 20:04:41 +0530

    adding screens merch home, cust home, wrapper update

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return StreamProvider<User>.value(
-    //   value: AuthService().user,
-    //   child: MaterialApp(
-    //     home: Wrapper(),
-    //   ),
-    // );
-    return MaterialApp(
-      home: LogIn(),
-      debugShowCheckedModeBanner: false,
-    );
+    return StreamProvider<User>.value(
+        value: AuthService().user,
+     child: MaterialApp(
+         home: Wrapper(),
+       ),
+     );
   }
 }
