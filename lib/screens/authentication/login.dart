aacb3b6ec5ee1ac84a1c05199ff46ab87df978d2
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2021-05-22 20:19:03 +0530

    login page styling

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -27,126 +27,140 @@ class _LogInState extends State<LogIn> {
 
   @override
   Widget build(BuildContext context) {
-    return loading? Loading() : Scaffold(
-      appBar: AppBar(
-        title: Image(image: AssetImage('assets/images/SZ.png')),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Container(
-              constraints: BoxConstraints.expand(),
-              child: Column(
-                children: [
-                  Image(image: AssetImage('assets/images/Open Doodles.png'))
-                ],
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-              )),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-            child: Column(
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Image(image: AssetImage('assets/images/SZ.png')),
+              backgroundColor: Colors.white,
+              elevation: 0.0,
+              centerTitle: true,
+            ),
+            backgroundColor: Colors.white,
+            body: Stack(
               children: [
-                SizedBox(height: 30.0),
-                SizedBox(height: 50.0),
-                Text('Log In',
-                    style: TextStyle(
-                        color: col1,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat',
-                        fontSize: 30.0)),
-                Form(
-                  key: _formKey,
+                Container(
+                    constraints: BoxConstraints.expand(),
+                    child: Column(
+                      children: [
+                        Image(
+                            image: AssetImage('assets/images/Open Doodles.png'))
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                    )),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
                   child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                          decoration: InputDecoration(
-                            hintText: 'email',
-                          ),
-                          validator: (val) =>
-                              val.isEmpty ? 'Enter an email' : null,
-                          onChanged: (val) {
-                            setState(() {
-                              email = val;
-                            });
-                          }),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                          decoration: InputDecoration(
-                            hintText: 'password',
-                          ),
-                          validator: (val) => val.length < 6
-                              ? 'Enter a password having more than 6 characters'
-                              : null,
-                          obscureText: true,
-                          onChanged: (val) {
-                            setState(() {
-                              password = val;
-                            });
-                          }),
-                      SizedBox(height: 20.0),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Color(0xff557089),
-                        ),
-                        child: Text(
-                          'Login',
+                    children: [
+                      SizedBox(height: 70.0),
+                      Text('Log In.',
                           style: TextStyle(
-                              color: Colors.white,
+                              color: Colors.indigo[400],
+                              fontWeight: FontWeight.bold,
                               fontFamily: 'Montserrat',
-                              fontSize: 18.0),
+                              fontSize: 56.0)),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 20.0),
+                            TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'email',
+                                ),
+                                validator: (val) =>
+                                    val.isEmpty ? 'Enter an email' : null,
+                                onChanged: (val) {
+                                  setState(() {
+                                    email = val;
+                                  });
+                                }),
+                            SizedBox(height: 20.0),
+                            TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'password',
+                                ),
+                                validator: (val) => val.length < 6
+                                    ? 'Enter a password having more than 6 characters'
+                                    : null,
+                                obscureText: true,
+                                onChanged: (val) {
+                                  setState(() {
+                                    password = val;
+                                  });
+                                }),
+                            SizedBox(height: 40.0),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.indigo[400],
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 66.0, vertical: 12.0),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10)))),
+                              child: Text('Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  dynamic result = await _auth.logInWithEmail(
+                                      email, password);
+                                  setState(() {
+                                    loading = true;
+                                  });
+                                  if (result == null) {
+                                    setState(() {
+                                      loading = false;
+                                    });
+                                    setState(() {
+                                      error =
+                                          'could not sign in with those credentials';
+                                    });
+                                  } else {
+                                    Navigator.push(
+                                      context,
+                                      // ignore: unnecessary_statements
+                                      MaterialPageRoute(
+                                          builder: (context) => CustomerHome()),
+                                    );
+                                  }
+                                }
+                              },
+                            ),
+                            SizedBox(
+                              height: 100,
+                            ),
+                            TextButton.icon(
+                              label: Text('New Account? Sign In here'),
+                              icon: Icon(Icons.person_add),
+                              onPressed: () {
+                                Navigator.pop(
+                                  context,
+                                );
+                              },
+                            ),
+                            Text(
+                              error,
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 14.0),
+                            )
+                          ],
                         ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            dynamic result = await _auth.logInWithEmail(email, password);
-                            setState(() {
-                              loading = true;
-                            });
-                            if (result == null) {
-                              setState(() {
-                                loading = false;
-                              });
-                              setState(() {
-                                error =
-                                    'could not sign in with those credentials';
-                              }
-                              );
-                            }
-                            else {
-
-                            Navigator.push(context,
-                            // ignore: unnecessary_statements
-                            MaterialPageRoute(builder: (context) => CustomerHome()),
-                            );
-                            }
-                          }
-                        },
-                      ),
-                      TextButton.icon(
-                        label: Text('New Account? Sign In here'),
-                        icon: Icon(Icons.person_add),
-                        onPressed: () {
-                          Navigator.pop(
-                            context,
-                          );
-                        },
-                      ),
-                      Text(
-                        error,
-                        style: TextStyle(color: Colors.red, fontSize: 14.0),
                       )
                     ],
                   ),
                 )
               ],
             ),
-          )
-        ],
-      ),
-    );
+          );
   }
 }
--- lib/screens/authentication/signin.dart
+++ lib/screens/authentication/signin.dart
@@ -3,11 +3,10 @@ import 'package:stealthzero/screens/authentication/login.dart';
 import 'package:stealthzero/services/auth.dart';
 import 'package:stealthzero/shared/load.dart';
 
-
 class SignIn extends StatefulWidget {
   //final Function toggleView;
 
- //ignIn({this.toggleView});
+  //ignIn({this.toggleView});
 
   @override
   _SignInState createState() => _SignInState();
@@ -25,101 +24,131 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-        child: Column(
-          children: [
-            SizedBox(height: 30.0),
-            Image(image: AssetImage('assets/images/SZlogin.png')),
-            SizedBox(height: 20.0),
-            Text(
-              'Sign Up for StealthZero',
-              style: TextStyle(
-                  color: Color(0xff557089),
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Montserrat',
-                  fontSize: 30.0),
-              textAlign: TextAlign.center,
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Image(image: AssetImage('assets/images/SZ.png')),
+              backgroundColor: Colors.white,
+              elevation: 0.0,
+              centerTitle: true,
             ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'email',
-                    ),
-                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                      onChanged: (val) {
-                        setState(() {
-                          email = val;
-                        });
-                      }),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'password',
-                    ),
-                      validator: (val) => val.length<6? 'Enter a password having more than 6 characters' : null,
-                      obscureText: true,
-                      onChanged: (val) {
-                        setState(() {
-                          password = val;
-                        });
-                      }),
-                  SizedBox(height: 10.0),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xff557089),
-                    ),
-                    child: Text(
-                      'Register',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Montserrat',
-                          fontSize: 18.0),
-                    ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        setState(() {
-                          loading = true;
-                        });
-                        dynamic result = await _auth.signUp(email, password);
-                        if(result == null){
-                          setState(() {
-                            loading = false;
-                          });
-                          setState(() {
-                            error = 'please supply a valid email';
-                          });
-
-                        }
-                      }
-                    },
+            backgroundColor: Colors.white,
+            body: Stack(
+              children: [
+                Container(
+                    constraints: BoxConstraints.expand(),
+                    child: Column(
+                      children: [
+                        Image(
+                            image: AssetImage('assets/images/Open Doodles.png'))
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                    )),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 30.0),
+                      Image(image: AssetImage('assets/images/SZlogin.png')),
+                      SizedBox(height: 20.0),
+                      Text(
+                        'Sign Up for StealthZero',
+                        style: TextStyle(
+                            color: Color(0xff557089),
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                            fontSize: 30.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 20.0),
+                            SizedBox(height: 10.0),
+                            TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'email',
+                                ),
+                                validator: (val) =>
+                                    val.isEmpty ? 'Enter an email' : null,
+                                onChanged: (val) {
+                                  setState(() {
+                                    email = val;
+                                  });
+                                }),
+                            SizedBox(height: 10.0),
+                            TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'password',
+                                ),
+                                validator: (val) => val.length < 6
+                                    ? 'Enter a password having more than 6 characters'
+                                    : null,
+                                obscureText: true,
+                                onChanged: (val) {
+                                  setState(() {
+                                    password = val;
+                                  });
+                                }),
+                            SizedBox(height: 10.0),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xff557089),
+                              ),
+                              child: Text(
+                                'Register',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 18.0),
+                              ),
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  setState(() {
+                                    loading = true;
+                                  });
+                                  dynamic result =
+                                      await _auth.signUp(email, password);
+                                  if (result == null) {
+                                    setState(() {
+                                      loading = false;
+                                    });
+                                    setState(() {
+                                      error = 'please supply a valid email';
+                                    });
+                                  }
+                                }
+                              },
+                            ),
+                            TextButton.icon(
+                              label:
+                                  Text('Already have an account? Login here'),
+                              icon: Icon(Icons.login),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LogIn()),
+                                );
+                              },
+                            ),
+                            Text(
+                              error,
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 14.0),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  TextButton.icon(
-                    label: Text('Already have an account? Login here'),
-                    icon: Icon(Icons.login),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LogIn()),
-                      );
-                    },
-                  ),
-                  Text(error,
-                  style: TextStyle(color: Colors.red, fontSize:14.0),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                ),
+              ],
+            ),
+          );
   }
 }
