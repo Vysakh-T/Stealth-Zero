e029496e8885796faffa6bce4a171b47a34629e2
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2021-05-22 19:49:07 +0530

    login signin style

--- lib/screens/authentication/log_signin.dart
+++ lib/screens/authentication/log_signin.dart
@@ -26,33 +26,54 @@ class Home extends StatelessWidget {
                     height: 450,
                   ),
                   ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LogIn()),
-                        );
-                      },
-                      child: Text('Log In',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ))),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => SignIn()),
-                        );
-                      },
-                      child: Text('Sign In',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ))),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LogIn()),
+                      );
+                    },
+                    child: Text('Log In',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        )),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.indigo[400],
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 60.0, vertical: 16.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10)))),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignIn()),
+                      );
+                    },
+                    child: Text('Sign In',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w700,
+                          color: Colors.indigo[400],
+                        )),
+                    style: OutlinedButton.styleFrom(
+                        primary: Colors.indigo[400],
+                        backgroundColor: Colors.grey[100],
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 56.0, vertical: 16.0),
+                        side: BorderSide(color: Colors.indigo[400], width: 3.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10)))),
+                  ),
                 ],
               )),
         ],
