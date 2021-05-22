ba35195f6bdddd7b60db882d593f22408c6330cd
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2021-05-22 13:37:23 +0530

    random

--- lib/screens/authentication/log_signin.dart
+++ lib/screens/authentication/log_signin.dart
@@ -10,14 +10,27 @@ class Home extends StatelessWidget {
         children: [
           Container(
               constraints: BoxConstraints.expand(),
+              child: Image(
+                image: AssetImage('assets/images/logsign.png'),
+              )),
+          Container(
+              constraints: BoxConstraints.expand(),
               child: Column(
-                children: [
-                  Image(image: AssetImage('assets/images/Vector-3.png')),
-                  Image(
-                      image: AssetImage('assets/images/Open Doodleslogin.png'))
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 150,
+                  ),
+                  ElevatedButton(
+                      onPressed: null,
+                      child: Text('Log In',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          )))
+                ],
               )),
         ],
       ),
